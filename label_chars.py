d69669fc049ae690dc20eae38bdb8e4cfb23fed5
Oliver Proud <owproud@gmail.com>
2020-04-09 19:05:36 +0100

    clean up label_chars.py

--- label_chars.py
+++ label_chars.py
@@ -11,9 +11,9 @@ import cv2
 import os
 
 
-model_filename = "captcha_model_fake_data.hdf5"
+model_filename = "models/captcha_model_98.hdf5"
 model_labels_filename = "labels.dat"
-image_folder = "/Users/oliverproud/howsmy-working/extracted_chars_test/"
+image_folder = "trainset_chars/"
 
 
 with open(model_labels_filename, "rb") as f:
@@ -23,14 +23,6 @@ with open(model_labels_filename, "rb") as f:
 model = load_model(model_filename)
 
 captcha_images = list(paths.list_images(image_folder))
-# captcha_images = np.random.choice(captcha_images, size=(10,), replace=False)
-
-
-# This function renames a file in the destination path
-# if the file that's being moved to the destination already exists
-def rename_file(dest, filename):
-    if os.path.exists(dest + "/" + filename):
-        os.rename(dest + "/" + filename, dest + "/" + new_name + ".png")
 
 
 num_correct = 0
@@ -58,84 +50,17 @@ for image_file in captcha_images:
     predicted_label = "".join(predictions)
 
     filename  = Path(image_file).stem + Path(image_file).suffix
-    new_name = str(random.randint(5000, 1000000))
 
-    if int(predicted_label) == 0:
-        src = image_file
-        dest = "/Users/oliverproud/howsmy-working/extracted_chars_test/test/0"
+    src = image_file
+    dest = "trainset_chars/labeled/"
+    if not os.path.exists(dest):
+        os.mkdir(dest)
 
-        rename_file(dest, filename)
+    for num in range(10):
+        if int(predicted_label) == num:
+            dest = dest + str(num)
 
-        dest = shutil.move(src, dest)
+            if not os.path.exists(dest):
+                os.mkdir(dest)
 
-    elif int(predicted_label) == 1:
-        src = image_file
-        dest = "/Users/oliverproud/howsmy-working/extracted_chars_test/test/1"
-
-        rename_file(dest, filename)
-
-        dest = shutil.move(src, dest)
-
-    elif int(predicted_label) == 2:
-        src = image_file
-        dest = "/Users/oliverproud/howsmy-working/extracted_chars_test/test/2"
-
-        rename_file(dest, filename)
-
-        dest = shutil.move(src, dest)
-
-    elif int(predicted_label) == 3:
-        src = image_file
-        dest = "/Users/oliverproud/howsmy-working/extracted_chars_test/test/3"
-
-        rename_file(dest, filename)
-
-        dest = shutil.move(src, dest)
-
-    elif int(predicted_label) == 4:
-        src = image_file
-        dest = "/Users/oliverproud/howsmy-working/extracted_chars_test/test/4"
-
-        rename_file(dest, filename)
-
-        dest = shutil.move(src, dest)
-
-    elif int(predicted_label) == 5:
-        src = image_file
-        dest = "/Users/oliverproud/howsmy-working/extracted_chars_test/test/5"
-
-        rename_file(dest, filename)
-
-        dest = shutil.move(src, dest)
-
-    elif int(predicted_label) == 6:
-        src = image_file
-        dest = "/Users/oliverproud/howsmy-working/extracted_chars_test/test/6"
-
-        rename_file(dest, filename)
-
-        dest = shutil.move(src, dest)
-
-    elif int(predicted_label) == 7:
-        src = image_file
-        dest = "/Users/oliverproud/howsmy-working/extracted_chars_test/test/7"
-
-        rename_file(dest, filename)
-
-        dest = shutil.move(src, dest)
-
-    elif int(predicted_label) == 8:
-        src = image_file
-        dest = "/Users/oliverproud/howsmy-working/extracted_chars_test/test/8"
-
-        rename_file(dest, filename)
-
-        dest = shutil.move(src, dest)
-
-    elif int(predicted_label) == 9:
-        src = image_file
-        dest = "/Users/oliverproud/howsmy-working/extracted_chars_test/test/9"
-
-        rename_file(dest, filename)
-
-        dest = shutil.move(src, dest)
+            dest = shutil.move(src, dest)
